599710cdeb0b87be74f387c24e90284baea4678a
JasonOct <59797161+JasonOct@users.noreply.github.com>
2021-08-22 21:01:25 +0800

    Bot commands fixed (except remove my alerts)

--- bot/main.py
+++ bot/main.py
@@ -2,19 +2,24 @@ import os, random, asyncio
 import discord
 from discord.ext import commands
 import httpx
-from discord_slash import SlashCommand, SlashContext
+from tabulate import tabulate
+import numpy as np
 
 client = commands.Bot(command_prefix="$")
-slash = SlashCommand(client, sync_commands=True)
 
 coingecko = "https://api.coingecko.com/api/v3/simple/price?ids=plant-vs-undead-token&vs_currencies=usd,bnb"
-WAIT_DURATION = int(os.environ['WAIT_DURATION'])
-TOKEN = os.environ['DISCORD_TOKEN']
+WAIT_DURATION = 10 #int(os.environ['WAIT_DURATION'])
+TOKEN = 'ODc4NTQwNDYwMTk1NDU5MTYy.YSCqkA.LHpC313lRjFhUjy6fx65z62m4X0' #os.environ['DISCORD_TOKEN']
+alertValues = []
+alertValuesUsers = []
+alertValuesUsersIDs = []
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     print(f"$ {getPvuPerUSD()} - BNB {getPvuPerBNB()}")
+    await client.get_channel(877632850130776084).send("Bot restarted. All price alerts have been removed.")
     asyncio.create_task(taskUpdateActivity())
 
 
@@ -47,6 +52,82 @@ async def taskUpdateActivity():
         activityStatus = "{:.2f}".format(getPvuPerBNB()) + " BNB"
 
         await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=activityStatus))
+        
+        currentPVUUSDT = getPvuPerUSD()
+        i = 0;
+        for x in alertValues:
+            if previousPVUUSDT > currentPVUUSDT:
+                if float(x) >= currentPVUUSDT:
+                    userToBeMentioned = client.get_user_info(int(alertValuesUsersIDs(x)))
+                    client.get_channel(877632850130776084).send(userToBeMentioned.mention + ", your target price at " + "${:.2f}".format(float(x)) + " has been reached. Current PVU/USDT rate: " + "${:.2f}".format(currentPVUUSDT))
+                    alertValues.remove(i)
+                    alertValuesUsers.remove(i)
+                    alertValuesUsersIDs.remove(i)
+            elif previousPVUUSDT < currentPVUUSDT:
+                if float(x) <= currentPVUUSDT:
+                    userToBeMentioned = client.get_user_info(int(alertValuesUsersIDs(x)))
+                    client.get_channel(877632850130776084).send(userToBeMentioned.mention + ", your target price at " + "${:.2f}".format(float(x)) + " has been reached. Current PVU/USDT rate: " + "${:.2f}".format(currentPVUUSDT))
+                    alertValues.remove(i)
+                    alertValuesUsers.remove(i)
+                    alertValuesUsersIDs.remove(i)
+
+        previousPVUUSDT = currentPVUUSDT
         await asyncio.sleep(WAIT_DURATION)
+        
+
+@client.command(
+	help="Looks like you need some help.",
+	brief="Sets a price alert"
+)
+async def alert(ctx, targetValue):
+    alertValues.append(targetValue)
+    print(alertValues)
+    alertValuesUsers.append(ctx.message.author.name)
+    print(alertValuesUsers)
+    alertValuesUsersIDs.append(str(ctx.message.author.id))
+    print(alertValuesUsersIDs)
+    await ctx.channel.send("{} PVU/USDT price alert set at $".format(ctx.message.author.mention) + targetValue)
+    
+@client.command(
+	help="Looks like you need some help.",
+	brief="Show current price alerts"
+)
+async def showalerts(ctx):
+    dataToPrint = zip(alertValues, alertValuesUsers)
+    await ctx.channel.send(tabulate(dataToPrint, headers=["Price Alert", "Set by"]))
+    
+@client.command(
+	help="Looks like you need some help.",
+	brief="Remove price alerts set by you."
+)
+async def removemyalerts(ctx):
+    deletedAlertValues = []
+    deletedAlertValuesUsers = []
+    deletedAlertValuesUsersIDs = []
+    i = 0;
+    for x in alertValuesUsersIDs:
+        if int(x) == ctx.message.author.id:
+            deletedAlertValues.append( alertValues[i] )
+            deletedAlertValuesUsers.append( alertValuesUsers[i] )
+            deletedAlertValuesUsersIDs.append( alertValuesUsersIDs[i] )
+            alertValues.remove(i)
+            alertValuesUsers.remove(i)
+            alertValuesUsersIDs.remove(i)
+        
+        i = i+1;
+    
+    dataToPrint = [deletedAlertValues, deletedAlertValuesUsers]
+    await ctx.channel.send("The following price alerts were removed: \n" + tabulate(dataToPrint, headers=["Price Alert", "Set by"]))
+
+@client.command(
+	help="Looks like you need some help.",
+	brief="Remove all price alerts."
+)
+async def removeallalerts(ctx):
+    alertValues.clear()
+    alertValuesUsers.clear()
+    alertValuesUsersIDs.clear()
+    await ctx.channel.send("All price alerts have been removed.")
+    
 
 client.run(TOKEN)
